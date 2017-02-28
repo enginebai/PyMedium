3c2ef9e961bb83d159af2fe1a38c3ac7c98e0844
enginebai <enginebai@gmail.com>
2017-02-28 09:43:33 +0800

    Split endpoints into api/parser modules; remove arts

--- pymedium/parser.py
+++ pymedium/parser.py
@@ -1,62 +1,9 @@
 #!/usr/bin/python
-# -*- encoding: utf-8 -*-
+# -*- coding: utf8 -*-
+from .model import User, Post, Publication, Tag, Image
+from .api import ROOT_URL
 
-import json
-
-from flask import Flask, jsonify, Response
-import requests
-
-from .model import *
-
-ROOT_URL = "https://medium.com/"
-ESCAPE_CHARACTERS = "])}while(1);</x>"
-ACCEPT_HEADER = {"Accept": "application/json"}
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return "Hello!!"
-
-
-@app.route("/<username>", methods=["GET"])
-def get_user_profile(username):
-    return send_request(ROOT_URL + "@{0}/latest".format(username), parse_function=parse_user)
-
-
-@app.route("/<username>/posts", methods=["GET"])
-def get_user_posts(username):
-    return send_request(ROOT_URL + "@{0}/latest".format(username), parse_function=parse_post)
-
-
-@app.route("/top")
-def get_top_posts():
-    return send_request(ROOT_URL + "browse/top", parse_function=parse_post)
-
-
-@app.route("/tags/<tag_name>", methods=["GET"])
-def get_top_posts_by_tag(tag_name):
-    return send_request(ROOT_URL + "tag/{tag}".format(tag=tag_name),
-                        parse_function=parse_post_from_search_by_tags)
-
-
-@app.route("/tags/<tag_name>/latest", methods=["GET"])
-def get_latest_posts_by_tag(tag_name):
-    return send_request(ROOT_URL + "tag/{tag}/latest".format(tag=tag_name),
-                        parse_function=parse_post_from_search_by_tags)
-
-
-def send_request(url, headers=ACCEPT_HEADER, param=None, parse_function=None):
-    req = requests.get(url, headers=headers, params=param)
-    req.encoding = "utf8"
-    if req.status_code == requests.codes.ok:
-        if parse_function is None:
-            parse_function = parse_post
-        model_dict = parse_function(json.loads(req.text.replace(ESCAPE_CHARACTERS, "").strip()))
-        return jsonify(model_dict)
-    else:
-        return Response(status=req.status_code)
+__author__ = 'enginebai'
 
 
 def parse_user(payload):
@@ -216,12 +163,3 @@ def parse_images(image_dict):
                     height=image.original_height,
                     id=image.image_id)
         return image.__dict__
-
-
-@app.route("/posts/<post_id>", methods=["GET"])
-def get_post(post_id):
-    pass
-
-
-if __name__ == "__main__":
-    get_user_profile("enginebai")
